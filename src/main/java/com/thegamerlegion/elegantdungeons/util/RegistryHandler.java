c00a3150db09df547dddaf50bed99ad45a4006be
Nicholas Garrell <nag5130@rit.edu>
2020-06-08 18:13:39 -0400

    Added boss keys and crystals to registered items, made json files for them, moved t4_crystal.png to the blocks folder as it is a block

--- src/main/java/com/thegamerlegion/elegantdungeons/util/RegistryHandler.java
+++ src/main/java/com/thegamerlegion/elegantdungeons/util/RegistryHandler.java
@@ -23,6 +23,16 @@ public class RegistryHandler {
     public static final RegistryObject<Item> KEY_DIAMOND = ITEMS.register("key_diamond", ItemBase::new);
     public static final RegistryObject<Item> KEY_IRON = ITEMS.register("key_iron", ItemBase::new);
     public static final RegistryObject<Item> KEY_GOLD = ITEMS.register("key_gold", ItemBase::new);
+    public static final RegistryObject<Item> KEY_T1_BOSS = ITEMS.register("key_t1_boss", ItemBase::new);
+    public static final RegistryObject<Item> KEY_T2_BOSS = ITEMS.register("key_t2_boss", ItemBase::new);
+    public static final RegistryObject<Item> KEY_T3_BOSS = ITEMS.register("key_t3_boss", ItemBase::new);
+    public static final RegistryObject<Item> KEY_T4_BOSS = ITEMS.register("key_t4_boss", ItemBase::new);
+    public static final RegistryObject<Item> KEY_T5_BOSS = ITEMS.register("key_t5_boss", ItemBase::new);
+    public static final RegistryObject<Item> T1_CRYSTAL_SHARD = ITEMS.register("t1_crystal_shard", ItemBase::new);
+    public static final RegistryObject<Item> T2_CRYSTAL_SHARD = ITEMS.register("t2_crystal_shard", ItemBase::new);
+    public static final RegistryObject<Item> T3_CRYSTAL_SHARD = ITEMS.register("t3_crystal_shard", ItemBase::new);
+    public static final RegistryObject<Item> T4_CRYSTAL_SHARD = ITEMS.register("t4_crystal_shard", ItemBase::new);
+    public static final RegistryObject<Item> T5_CRYSTAL_SHARD = ITEMS.register("t5_crystal_shard", ItemBase::new);
 
     // Blocks
 
